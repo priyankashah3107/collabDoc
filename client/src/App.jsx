e0466e6634642f36720ac1f76c38795780aaf829
Priyanka <priyankashah3107@gmail.com>
2024-03-09 21:23:51 +0530

    Generated unique id for every client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 
 import TextEditor from './components/TextEditor'
+import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 
 function App() {
@@ -7,7 +9,14 @@ function App() {
 
   return (
     <> 
-   <TextEditor />
+
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidv4()}`} replace />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </BrowserRouter>
+  
     </>
   )
 }
