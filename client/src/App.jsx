ca65dc5c9f1351d32188ab60961fb6b0ae3f793e
Priyanka <priyankashah3107@gmail.com>
2024-03-09 14:52:39 +0530

    project setup + quill textEditor

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import TextEditor from './components/TextEditor'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <> 
+   <TextEditor />
+    </>
+  )
+}
+
+export default App
